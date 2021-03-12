39c450d30a06f54e09aa7ce4020e7ccaddf3a40f
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-03-12 23:32:22 +0100

    Update Contracts

--- contracts/index.js
+++ contracts/index.js
@@ -1,5 +1,18 @@
 import usdc from './usdc'
 import governor from './governor'
-import token from './token'
+import dollar from './dollar'
+import stake from './stake'
+import oldDao from './oldDao'
+import oldDollar from './oldDollar'
 
-export default { usdc, governor, token }
+let contracts = { usdc, governor, dollar, stake, oldDao, oldDollar }
+
+// Stake Deployed to: 0x3332f56a061cf2b4172A9DE58253AB21B4331818
+// Dollar Deployed to: 0x0B283FBD3475CBf6825B3a112958Ee9b42Dfcd2b
+// Registry Deployed to: 0xe13DbE3Ac039C2ca815A885017654aEd31F7B12A
+// GovernorAlpha Deployed to: 0xD03a54cAdE6d8C4fe9e1D9e777bDcFC425950efc
+
+contracts.governor.address = '0xD03a54cAdE6d8C4fe9e1D9e777bDcFC425950efc'
+contracts.dollar.address = '0x0B283FBD3475CBf6825B3a112958Ee9b42Dfcd2b'
+
+export default contracts
---
+++ contracts/oldDao.js
@@ -0,0 +1,71 @@
+const address = '0xB36e89396C027fa54139d4c5647Dc936114B8859'
+
+const abi = [
+  {
+    constant: true,
+    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'totalCouponUnderlying',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'newTotalCouponUnderlying',
+        type: 'uint256',
+      },
+    ],
+    name: 'set',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'address', name: 'account', type: 'address' },
+      { internalType: 'uint256', name: 'balance', type: 'uint256' },
+    ],
+    name: 'addUserBalance',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'address', name: 'account', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    ],
+    name: 'burn',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
+export default { address, abi }
