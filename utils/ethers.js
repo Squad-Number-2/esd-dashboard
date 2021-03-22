8d19fb7e972e35d98a38584b22855690d521563a
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-03-22 14:45:29 +0100

    Add generic erc20 approver

--- utils/ethers.js
+++ utils/ethers.js
@@ -6,6 +6,8 @@ export let web3 = new ethers.providers.InfuraProvider(
   process.env.INFURA
 )
 
+const MaxUint = ethers.constants.MaxUint256
+
 export const registerProvider = (wallet) => {
   if (wallet) {
     console.log('Using Wallet provider')
@@ -13,6 +15,45 @@ export const registerProvider = (wallet) => {
   } else if (window && window.ethereum) {
     console.log('Using Window provider')
     web3 = new ethers.providers.Web3Provider(window.ethereum)
+  }
+}
+
+export const setApproval = async (contract, spender, amount) => {
+  const signer = web3.getSigner()
+
+  const erc20 = new ethers.Contract(
+    contract,
+    [
+      {
+        constant: false,
+        inputs: [
+          {
+            name: '_spender',
+            type: 'address',
+          },
+          {
+            name: '_value',
+            type: 'uint256',
+          },
+        ],
+        name: 'approve',
+        outputs: [
+          {
+            name: '',
+            type: 'bool',
+          },
+        ],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+    ],
+    signer
+  )
+  try {
+    return await erc20.approve(spender, amount ? amount : MaxUint)
+  } catch (error) {
+    return error
   }
 }
 
