690c9a7587a56c8811317d740127d4b39e167649
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-04-05 17:38:37 +0200

    Added back button to header

--- components/page/index.js
+++ components/page/index.js
@@ -1,33 +1,54 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 import Header from './header'
 import Footer from './footer'
-import { Flex, Box, Text, Heading, Skeleton } from '@chakra-ui/react'
+import {
+  Flex,
+  Box,
+  Text,
+  Heading,
+  Skeleton,
+  IconButton,
+} from '@chakra-ui/react'
+import { ArrowBackIcon } from '@chakra-ui/icons'
 
-const Page = ({ children, header, subheader }) => (
-  <Flex direction="column" minHeight="100vh">
-    <Head>
-      <title>Empty Set Dollar</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Header />
-    <Flex bg="black" color="white" p="5em 3em 10em" justify="center">
-      <Box width="100%" maxW={'1200px'}>
-        <Skeleton isLoaded={header} mb="20px">
-          <Heading fontSize="4xl">{header}</Heading>
-        </Skeleton>
-        <Skeleton isLoaded={subheader}>
-          <Text fontSize="lg">{subheader}</Text>
-        </Skeleton>
-      </Box>
+const Page = ({ children, header, subheader, back }) => {
+  const router = useRouter()
+  return (
+    <Flex direction="column" minHeight="100vh">
+      <Head>
+        <title>Empty Set Dollar</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Flex bg="black" color="white" p="5em 3em 10em" justify="center">
+        {back ? (
+          <IconButton
+            icon={<ArrowBackIcon w={8} h={8} />}
+            variant="ghost"
+            colorScheme="white"
+            onClick={() => router.back()}
+          />
+        ) : null}
+
+        <Box width="100%" maxW={'1200px'}>
+          <Skeleton isLoaded={header} mb="20px">
+            <Heading fontSize="4xl">{header}</Heading>
+          </Skeleton>
+          <Skeleton isLoaded={subheader}>
+            <Text fontSize="lg">{subheader}</Text>
+          </Skeleton>
+        </Box>
+      </Flex>
+      <Flex flexGrow="1" p="0em 3em 0em" justify="center">
+        <Box maxW={'1200px'} w="100%">
+          {children}
+        </Box>
+      </Flex>
+      <Footer />
     </Flex>
-    <Flex flexGrow="1" p="0em 3em 0em" justify="center">
-      <Box maxW={'1200px'} w="100%">
-        {children}
-      </Box>
-    </Flex>
-    <Footer />
-  </Flex>
-)
+  )
+}
 
 export default Page
