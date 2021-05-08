e00c386dbcb1fc8f4e569a18f9c0141e9d882458
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-05-08 13:42:25 +0200

    Clean dead code

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,8 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { Web3Provider } from '../contexts/useWeb3'
 import { useWallet, UseWalletProvider } from 'use-wallet'
 import { AlertProvider } from '../contexts/useAlerts'
 
 import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/react'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: "Helvetica", sans-serif, monospace;
-    background: black;
-    min-height: 100vh;
-    a {
-      text-decoration: none;
-      color: #424242;
-    }
-  }
-`
 
 const theme = {
   styles: {
@@ -40,7 +24,6 @@ export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={extendTheme({ theme })}>
       <CSSReset />
-      <GlobalStyle />
       <UseWalletProvider
         chainId={3}
         connectors={{
