06ac1adccf50fb8bbfee7935ec650e30624ece78
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-07-30 16:18:41 +0200

    Update contracts and mainnet addresses

--- contracts/index.js
+++ contracts/index.js
@@ -1,39 +1,41 @@
-import usdc from './usdc'
-import governor from './governor'
-import dollar from './dollar'
-import stake from './stake'
-import oldDao from './oldDao'
-import oldDollar from './oldDollar'
-import reserve from './reserve'
-import migrator from './migrator'
-import proxyAdmin from './proxyAdmin'
-import registry from './registry'
-import incentivisor from './incentivisor'
+import DollarABI from './Dollar.json'
+import GovernorAlphaABI from './GovernorAlpha.json'
+import IncentivizerABI from './Incentivizer.json'
+import MigratorABI from './Migrator.json'
+import MockV1DAOABI from './MockV1DAO.json'
+import MockV1DollarABI from './MockV1Dollar.json'
+import ProxyImplABI from './ProxyImpl.json'
+import ProxyRootABI from './ProxyRoot.json'
+import RegistryABI from './Registry.json'
+import ReserveABI from './Reserve.json'
+import ReserveImplABI from './ReserveImpl.json'
+import StakeABI from './Stake.json'
+import TimelockABI from './Timelock.json'
+import VesterABI from './Vester.json'
+import usdcABI from './USDC.json'
 
-let contracts = {
-  usdc,
-  governor,
-  dollar,
-  stake,
-  oldDao,
-  oldDollar,
-  reserve,
-  migrator,
-  proxyAdmin,
-  registry,
-  iEsdUsdc: {
-    abi: incentivisor.abi,
-    address: '0x80560504210626426F9F53f6E0a3FFaDE647079f',
-    name: 'ESD/USD Incentivizor',
-  },
+import addresses from './addresses.json'
+
+const abi = {
+  DOLLAR: DollarABI,
+  STAKE: StakeABI,
+  TIMELOCK: TimelockABI,
+  REGISTRY: RegistryABI,
+  GOVERNORALPHA: GovernorAlphaABI,
+  PROXYIMPL: ProxyImplABI,
+  PROXYROOT: ProxyRootABI,
+  RESERVE: ReserveABI,
+  VESTER: VesterABI,
+  INCENTIVIZER: IncentivizerABI,
+  V1_DAO: MockV1DAOABI,
+  V1_DOLLAR: MockV1DollarABI,
+  MIGRATOR: MigratorABI,
+  USDC: usdcABI,
 }
 
-// Ropsten addresses
-contracts.stake.address = '0xe2b06fef1df17d00aabd091de62c7b39af950ecf'
-contracts.oldDao.address = '0xD783084469DaFc397a06bD7a5214D75Cf4F94707'
-contracts.oldDollar.address = '0xE0551610571E180FB6AE6bbEAD2e4225fe0cAb31'
-contracts.reserve.address = '0x9D8bA2B9602BaE36ee2F5F072f1c7Ab8945A94f2'
-contracts.governor.address = '0x986EDd56a8Ec0d1B6074ea9aeA52CE04396fA306'
-contracts.dollar.address = '0x2d61A9eF1F72A1E961C7Dea4f0E25bC7E6eB4Ea7'
-contracts.usdc.address = '0x07865c6E87B9F70255377e024ace6630C1Eaa37F'
+let contracts = {}
+
+Object.keys(abi).map((i) => {
+  contracts[i] = { address: addresses[i], abi: abi[i] }
+})
 export default contracts
