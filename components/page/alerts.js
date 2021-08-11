de05f910c47ff30dbed73555f9318358faf44533
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-08-11 12:04:22 +0200

    Format Alerts better

--- components/page/alerts.js
+++ components/page/alerts.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { Flex, Box, Image, Link } from '@chakra-ui/react'
+import {
+  Flex,
+  Box,
+  Image,
+  Link,
+  Icon,
+  useColorModeValue,
+  chakra,
+} from '@chakra-ui/react'
 import { CheckCircleIcon, WarningTwoIcon, TimeIcon } from '@chakra-ui/icons'
 
 import useAlerts from '../../contexts/useAlerts'
@@ -13,52 +21,128 @@ const Alerts = () => {
       right={['10', '0']}
       p="10px 30px"
       zIndex="10"
+      minWidth="300px"
     >
       {alerts.reverse().map((item, i) => {
         switch (item.type) {
           case 'pending':
             return (
-              <Box
-                borderWidth="1px"
-                borderColor="cream"
-                borderRadius="10px"
-                bg="white"
-                p="3px 20px 5px"
-                key={'alert' + i}
+              <Flex
+                maxW="sm"
+                w="full"
+                mx="auto"
+                bg={useColorModeValue('white', 'gray.800')}
+                shadow="md"
+                rounded="lg"
+                overflow="hidden"
+                mt="10px"
               >
-                <TimeIcon color="palevioletred" mt="-2px" mr="5px" />{' '}
-                {item.text} pending
-              </Box>
+                <Flex
+                  justifyContent="center"
+                  alignItems="center"
+                  w={12}
+                  bg="blue.500"
+                >
+                  <Icon as={TimeIcon} color="white" boxSize={6} />
+                </Flex>
+
+                <Box mx={-3} py={2} px={4}>
+                  <Box mx={3}>
+                    <chakra.span
+                      color={useColorModeValue('blue.500', 'blue.400')}
+                      fontWeight="bold"
+                    >
+                      Info
+                    </chakra.span>
+                    <chakra.p
+                      color={useColorModeValue('gray.600', 'gray.200')}
+                      fontSize="sm"
+                    >
+                      {item.text}
+                    </chakra.p>
+                  </Box>
+                </Box>
+              </Flex>
             )
             break
           case 'success':
             return (
-              <Box
-                borderWidth="1px"
-                borderColor="cream"
-                borderRadius="10px"
-                bg="white"
-                p="3px 20px 5px"
-                key={'alert' + i}
+              <Flex
+                maxW="sm"
+                w="full"
+                mx="auto"
+                bg={useColorModeValue('white', 'gray.800')}
+                shadow="md"
+                rounded="lg"
+                overflow="hidden"
+                mt="10px"
               >
-                <CheckCircleIcon color="green" mt="-2px" mr="5px" /> {item.text}{' '}
-                succeeded
-              </Box>
+                <Flex
+                  justifyContent="center"
+                  alignItems="center"
+                  w={12}
+                  bg="green.500"
+                >
+                  <Icon as={CheckCircleIcon} color="white" boxSize={6} />
+                </Flex>
+
+                <Box mx={-3} py={2} px={4}>
+                  <Box mx={3}>
+                    <chakra.span
+                      color={useColorModeValue('green.500', 'green.400')}
+                      fontWeight="bold"
+                    >
+                      Success
+                    </chakra.span>
+                    <chakra.p
+                      color={useColorModeValue('gray.600', 'gray.200')}
+                      fontSize="sm"
+                    >
+                      {item.text}!
+                    </chakra.p>
+                  </Box>
+                </Box>
+              </Flex>
             )
             break
           case 'fail':
             return (
-              <Box
-                borderWidth="1px"
-                borderColor="cream"
-                borderRadius="10px"
-                bg="white"
-                p="3px 20px 5px"
-                key={'alert' + i}
+              <Flex
+                maxW="sm"
+                w="full"
+                mx="auto"
+                bg={useColorModeValue('white', 'gray.800')}
+                shadow="md"
+                rounded="lg"
+                overflow="hidden"
+                mt="10px"
               >
-                <WarningTwoIcon color="red" mt="-2px" mr="5px" /> {item.text}{' '}
-                failed
-              </Box>
+                <Flex
+                  justifyContent="center"
+                  alignItems="center"
+                  w={12}
+                  bg="red.500"
+                >
+                  <Icon as={WarningTwoIcon} color="white" boxSize={6} />
+                </Flex>
+
+                <Box mx={-3} py={2} px={4}>
+                  <Box mx={3}>
+                    <chakra.span
+                      color={useColorModeValue('red.500', 'red.400')}
+                      fontWeight="bold"
+                    >
+                      Error
+                    </chakra.span>
+                    <chakra.p
+                      color={useColorModeValue('gray.600', 'gray.200')}
+                      fontSize="sm"
+                    >
+                      {item.text}!
+                    </chakra.p>
+                  </Box>
+                </Box>
+              </Flex>
             )
             break
           default:
